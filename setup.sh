7df17a7492aa2324ba19aa1532d0e3b5d251e9fb
adlioaki <31689420+adlioaki@users.noreply.github.com>
2021-05-18 00:31:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"zordonbonzo@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
-port = $PORT8080\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
